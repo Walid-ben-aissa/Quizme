933c106679061b146de265e168ca4996f435d9e6
walid-ben-aissa <walid2525@gmail.com>
2021-06-09 10:10:59 +0100

    v0.2

--- quizme/src/App.js
+++ quizme/src/App.js
@@ -1,32 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-
-class Timer extends React.Component{
-  state={
-    x:this.props.time
+class Timer extends React.Component {
+  state = {
+    x: parseFloat(this.props.time),
+  };
+  count = function () {
+    this.setState({ x: this.state.x - 0.01 });
+  };
+  componentDidMount() {
+    this.y = setInterval(this.count, 10);
   }
-  count(){
-    this.setState()
+  render() {
+    if (this.state.x < 0.001) clearInterval(this.y);
+    return <div>{Math.abs(this.state.x.toFixed(2))}</div>;
   }
-  componentDidMount(){
-    // setInterval(this.count,1000);
-  }
-  render(){
-    return(this.state.x)}
 }
 
-class Counter extends React.Component{
-render(){
-  return(<div><Timer time="30.0"/><br/>
-  <Timer time="60.0"/><br/>
-  <Timer time="120.0"/></div>)
-}
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <Timer time="30.0" />
+        <br />
+        <Timer time="60.0" />
+        <br />
+        <Timer time="120.0" />
+      </div>
+    );
+  }
 }
 function App() {
   return (
     <div>
-      <Counter/>
+      <Counter />
     </div>
   );
 }
