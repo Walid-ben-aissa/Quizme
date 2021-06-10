f23096aefff7d577f4dd85d1212364196887175a
walid-ben-aissa <walid2525@gmail.com>
2021-06-10 17:54:39 +0100

    v0.7

--- quizme/src/Categories/Categories.js
+++ quizme/src/Categories/Categories.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 
 class Categories extends React.Component {
   state = { x: "" };
-  render() {
+  getcats() {
     fetch("https://opentdb.com/api_category.php")
       .then((response) => response.json())
       .then((data) => {
         this.setState({ x: Object.values(data)[0] });
       });
-    this.state.x.map((val) => <li>{val}</li>);
-    console.log(this.state.x);
-    return <ul>{this.state.x}</ul>;
+  }
+  render() {
+    /*  console.log(y);
+    y.map((val) => <li>{val}</li>); */
+    console.log("this.state.x");
+    return null;
   }
 }
 export default Categories;
