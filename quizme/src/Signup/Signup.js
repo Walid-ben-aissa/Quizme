2b13c63ea1596af5b94ada318432af00a9dc2c26
walid-ben-aissa <walid2525@gmail.com>
2021-06-15 18:10:40 +0100

    v0.9

--- quizme/src/Signup/Signup.js
+++ quizme/src/Signup/Signup.js
@@ -7,16 +7,64 @@ import img3 from "./avatar3.png";
 import img4 from "./avatar4.png";
 import img5 from "./avatar5.png";
 import img6 from "./avatar6.png";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import { ButtonGroup } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+function ToggleButtonExample() {
+  const [radioValue, setRadioValue] = useState("1");
+
+  const radios = [
+    { name: "Radio", value: "1" },
+    { name: "Radio", value: "2" },
+    { name: "Radio", value: "3" },
+    { name: "Radio", value: "4" },
+    { name: "Radio", value: "5" },
+    { name: "Radio", value: "6" },
+  ];
+
+  return (
+    <>
+      <br />
+      <Row>
+        <Col>
+          <ButtonGroup className="mb-2">
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={idx}
+                id={`radio-${idx}`}
+                type="radio"
+                variant="secondary"
+                name="radio"
+                value={radio.value}
+                checked={radioValue === radio.value}
+                onChange={(e) => setRadioValue(e.currentTarget.value)}
+              >
+                <img src={img1} alt="av" className="img" />
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+        </Col>
+      </Row>
+      <br />
+    </>
+  );
+}
 
 class Avatar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { id: this.props.id, active: false };
+    this.state = {
+      id: this.props.id,
+      active: this.props.active === this.props.id,
+    };
+    console.log(this.props.active === this.props.id);
   }
   select = () => {
     this.setState({ active: !this.state.active });
     this.props.callback(this.state.id);
   };
+
   render() {
     return (
       <Button
@@ -32,8 +80,13 @@ class Avatar extends React.Component {
   }
 }
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activated: 5 };
+  }
   handleclick = (id) => {
-    console.log(id);
+    this.setState({ activated: id });
+    console.log(this.state.activated);
   };
   render() {
     return (
@@ -52,26 +105,57 @@ class Signup extends React.Component {
               Créer un compte
             </h1>
             <br />
+            <ToggleButtonExample />
             <Form>
               Choisir un avatar:
-              <Row >
+              <Row>
                 <Col>
-                  <Avatar img={img1} id="1" callback={this.handleclick} />
+                  <Avatar
+                    img={img1}
+                    id={1}
+                    callback={this.handleclick}
+                    active={this.state.activated}
+                  />
                 </Col>
                 <Col>
-                  <Avatar img={img3} id="2" callback={this.handleclick} />
+                  <Avatar
+                    img={img3}
+                    id={2}
+                    callback={this.handleclick}
+                    active={this.state.activated}
+                  />
                 </Col>
                 <Col>
-                  <Avatar img={img4} id="3" callback={this.handleclick} />
+                  <Avatar
+                    img={img4}
+                    id={3}
+                    callback={this.handleclick}
+                    active={this.state.activated}
+                  />
                 </Col>
                 <Col>
-                  <Avatar img={img2} id="4" callback={this.handleclick} />
+                  <Avatar
+                    img={img2}
+                    id={4}
+                    callback={this.handleclick}
+                    active={this.state.activated}
+                  />
                 </Col>
                 <Col>
-                  <Avatar img={img5} id="5" callback={this.handleclick} />
+                  <Avatar
+                    img={img5}
+                    id={5}
+                    callback={this.handleclick}
+                    active={this.state.activated}
+                  />
                 </Col>
                 <Col>
-                  <Avatar img={img6} id="6" callback={this.handleclick} />
+                  <Avatar
+                    img={img6}
+                    id={6}
+                    callback={this.handleclick}
+                    active={this.state.activated}
+                  />
                 </Col>
               </Row>
               <Form.Group>
