5783c56d7b8531538c6afc2d6af7cbca898aa8cc
walid-ben-aissa <walid2525@gmail.com>
2021-06-29 10:55:22 +0100

    removed useless things

--- quizme/src/App.js
+++ quizme/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-/* import Counter from "./Timer/Timer"; */
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 import Leaderboards from "./Leaderboards/Leaderboards";
